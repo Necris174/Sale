e24c9f9a5380069662397ab1eb77d5d00ebd67d4
Necris174 <plotnicovvyu@gmail.com>
2022-07-13 15:25:58 +0500

    sale2

--- src/Sale.java
+++ src/Sale.java
@@ -5,12 +5,15 @@ public class Sale {
                                     int discount,
                                     int offset,
                                     int readLength) {
-        if (discount > 0 && discount < 100 && offset >= 0 && readLength > 0) {
+        if (discount > 0 && discount < 100 && offset >= 0 && readLength > 0
+                && readLength+offset<=price.length) {
             int[] newPrices = new int[readLength];
             int j = 0;
             for (int i = offset; i < readLength + offset; i++) {
-                newPrices[j] = (int) Math.floor(price[i] - price[i] * discount / 100d);
-                j++;
+                if(price[i]>0){
+                    newPrices[j] = (int) Math.floor(price[i] - price[i] * discount / 100d);
+                    j++;
+                } else throw new IllegalArgumentException();
             }
             return newPrices;
         } else {
